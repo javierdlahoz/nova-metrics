65585d6b763fa7bfb94fb85b8411c65458f771ef
Javier De la Hoz <javierdlahoz@gmail.com>
2020-06-18 12:08:27 +0200

    fixing date parsing on trend series

--- resources/js/mixins/InteractWithDates.vue
+++ resources/js/mixins/InteractWithDates.vue
@@ -2,14 +2,17 @@
 export default {
     methods: {
         parseDate(date) {
+            let dateObject, formattedDate = date
             const year = (new Date()).getFullYear(), splitDate = date.split(' - ')
-            let formattedDate = date
-
+            
             if (splitDate.length > 1) {
                 formattedDate = `${splitDate[0]}, ${year.toString()} ${splitDate[1]}`
             }
 
-            return Date.parse(formattedDate)
+            dateObject = new Date(Date.parse(formattedDate))
+            dateObject.setDate(dateObject.getDate() + 1)
+
+            return dateObject.valueOf()
         }
     }
 }
